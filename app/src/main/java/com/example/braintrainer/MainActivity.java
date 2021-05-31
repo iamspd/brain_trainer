4c224074affbf60758d4d2d8979ef482b4a61b39
iamspd <iamspd.ai9@gmail.com>
2021-05-30 22:22:18 -0400

    MainActivity - Method onGoClick implemented

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // widgets
+    private Button btnGo;
+
+    // methods
+    public void onGoClick(View view){
+        btnGo.setVisibility(View.INVISIBLE);
+    }
+
+    private void findAllViews() {
+
+        btnGo = findViewById(R.id.btnGo);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findAllViews();
     }
+
 }
