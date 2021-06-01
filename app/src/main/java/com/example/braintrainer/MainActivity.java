90e1edfcc41b5a8df115306a14443bb7b927ce53
iamspd <iamspd.ai9@gmail.com>
2021-06-01 19:40:09 -0400

    MainActivity - Sum implemented

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     // widgets
     private Button btnGo;
+    private TextView tvSum;
+
+    // variables
+    private Random randomNumber;
+    private int firstNumber, secondNumber;
 
     // methods
     public void onGoClick(View view){
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private void findAllViews() {
 
         btnGo = findViewById(R.id.btnGo);
+        tvSum = findViewById(R.id.tvSum);
 
     }
 
@@ -28,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findAllViews();
+
+        // generate a random number having the limit for 20 numbers (0-20)
+        randomNumber = new Random();
+        firstNumber = randomNumber.nextInt(21);
+        secondNumber = randomNumber.nextInt(21);
+
+        String sum = firstNumber + " + " + secondNumber;
+
+        tvSum.setText(sum);
     }
 
 }
