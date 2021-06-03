24c72be62b71e50d570ba7044119205b9abe59af
iamspd <iamspd.ai9@gmail.com>
2021-06-02 21:15:07 -0400

    MainActivity - Sum text implemented

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     // variables
     private Random randomNumber;
     private int firstNumber, secondNumber;
+    private int locationOfCorrectAnswers, correctAnswer, inCorrectAnswers;
+    private ArrayList<Integer> sumAnswers;
 
     // methods
     public void onGoClick(View view){
@@ -43,9 +46,27 @@ public class MainActivity extends AppCompatActivity {
         firstNumber = randomNumber.nextInt(21);
         secondNumber = randomNumber.nextInt(21);
 
-        String sum = firstNumber + " + " + secondNumber;
+        correctAnswer = firstNumber + secondNumber;
 
-        tvSum.setText(sum);
+        String sumText = firstNumber + " + " + secondNumber;
+
+        tvSum.setText(sumText);
+
+        locationOfCorrectAnswers = randomNumber.nextInt(4);
+
+        for (int i = 0; i < 4; i++){
+
+            if (locationOfCorrectAnswers == i) {
+                sumAnswers.add(correctAnswer);
+            } else {
+
+                inCorrectAnswers = randomNumber.nextInt(41);
+
+                // sumAnswers.add()
+
+            }
+
+        }
     }
 
 }
